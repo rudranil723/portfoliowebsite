ca730e1e17344d7c75cacddbd6df2a8cd443f98f
Ruddy <rudranilshil@gmail.com>
2024-02-08 22:32:13 +0530

    home page okay

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react'
 import $ from 'jquery'; // Import jQuery
 import { Link } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.css';
@@ -8,9 +8,23 @@ import image2 from '../../assets/images/2.png';
 import image3 from '../../assets/images/3.jpeg';
 import image4 from '../../assets/images/4.jpeg';
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters'
 
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['u', 'd', 'r', 'a', 'n', 'i', 'l']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'l', 'o', 'p', 'e', 'r']
+
+    
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+        return () => clearTimeout(timeoutId);
+    }, []); 
+    
     useEffect(() => {
         // Place the jQuery code within the useEffect hook
         $(".option").click(function () {
@@ -21,13 +35,26 @@ const Home = () => {
     return (
         <div className="container home-page" >
             <div className="text-zone">
-                <h1>hi,<br/>I'm,
-                {/* we have to rmake the R photo but later */}
-                <img className = 'namelogo' src={LogoTitle} style={{width: '60px', height: '60px'}} alt="devloper"/>
-                udranil
-                <br/>
-                <span className='web-devloper-text'>web devloper.</span>
-            </h1>
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+
+                    <img className = 'namelogo' src={LogoTitle} style={{width: '60px', height: '60px'}} alt="devloper"/>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br/>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
+                </h1>
                 <h2>Frontend devloper/ software devloper</h2>
                 <Link to="/contact" className='flat-button'> CONTACT ME</Link>
             </div>
